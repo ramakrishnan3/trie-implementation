1e18b73ad9d8e63137e93d2b85a42e73890dc567
ramakrishnan3 <ramrocks4eva@gmail.com>
2020-04-17 00:17:19 +0530

    trie search with priority queue sort

--- src/search.js
+++ src/search.js
@@ -1,15 +1,13 @@
 var Trie = require("../../trie-ing");
-var input = require("./million");
-const { v4: uuidv4 } = require('uuid');/* 
-import Trie from "../../trie-ing";
-import input from "./sample";
-import { v1 as uuidv1 } from 'uuid'; */
+var input = require("./sample");
+const { v4: uuidv4 } = require('uuid');
+var fs = require('fs');
 
-var trie = new Trie();
+var trie = new Trie({maxWidth: 50});
 console.log("before length: ", new Date());
 console.log("length:", input.length);
 console.log("after length: ", new Date());
-input.forEach((item, i) => {
+input.reverse().forEach((item, i) => {
     // console.log(JSON.stringify(item));
     trie.add({
         id: uuidv4(),
@@ -20,7 +18,29 @@ input.forEach((item, i) => {
 });
 console.log("before search: ", new Date());
 
+// trie.root.sort();
+
+/* fs.writeFile('sample_trie.js',JSON.stringify(trie), function (err) {
+    if (err) throw err;
+    console.log('It\'s saved!', new Date());
+  }); */
+
+
+var results = [];
+var opts = {unique: true, limit:  input.length};
+
+console.log("before sorting: ", new Date());
+
+
+// trie.root.getSortedResults("notneeded", results, opts, new PQueue(opts.limit));
+
+console.log("after sorting: ", new Date());
+
+
+
+
+console.log("before 1 search: ", new Date());
 console.log(trie.prefixSearch('f', {limit: 3, unique: true})); 
-console.log("after search: ", new Date());
+console.log("before 2 search: ", new Date());
 console.log(trie.prefixSearch('f', {limit: 3, unique: true})); 
 console.log("after search: ", new Date());
