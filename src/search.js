db8de2fdf92b08f614d0d26bf00c11f69e36b9db
ramakrishnan3 <ramrocks4eva@gmail.com>
2020-04-17 03:05:53 +0530

    serialize and deserialize

--- src/search.js
+++ src/search.js
@@ -1,46 +1,36 @@
 var Trie = require("../../trie-ing");
-var input = require("./sample");
-const { v4: uuidv4 } = require('uuid');
+var input = require("./million");
+var output = require("../million_trie");
 var fs = require('fs');
 
-var trie = new Trie({maxWidth: 50});
-console.log("before length: ", new Date());
-console.log("length:", input.length);
-console.log("after length: ", new Date());
-input.reverse().forEach((item, i) => {
-    // console.log(JSON.stringify(item));
-    trie.add({
-        id: uuidv4(),
-        key: item.value.name,
-        value: {name: item.value.name},
-        score: item.score
+if (!output.root) {
+    console.log("from scratch start: ", new Date());
+    var trie = new Trie({maxWidth: 50});
+    input.forEach((item, i) => {
+        trie.add({
+            key: item.value.name,
+            value: {name: item.value.name},
+            score: item.score
+        });
     });
-});
-console.log("before search: ", new Date());
+    console.log("from scratch end: ", new Date());
 
-// trie.root.sort();
+} else {
+    console.log("from json start: ", new Date());
+    var trie = new Trie(output);
+    console.log("from json end: ", new Date());
+}
 
-/* fs.writeFile('sample_trie.js',JSON.stringify(trie), function (err) {
+
+fs.writeFile('million_trie.js', `const output = ${JSON.stringify(trie)}; module.exports = output;`, function (err) {
     if (err) throw err;
     console.log('It\'s saved!', new Date());
-  }); */
-
-
-var results = [];
-var opts = {unique: true, limit:  input.length};
-
-console.log("before sorting: ", new Date());
+});
 
 
 // trie.root.getSortedResults("notneeded", results, opts, new PQueue(opts.limit));
 
-console.log("after sorting: ", new Date());
 
 
-
-
-console.log("before 1 search: ", new Date());
 console.log(trie.prefixSearch('f', {limit: 3, unique: true})); 
-console.log("before 2 search: ", new Date());
-console.log(trie.prefixSearch('f', {limit: 3, unique: true})); 
-console.log("after search: ", new Date());
+console.log(trie.prefixSearch('fa', {limit: 3, unique: true}));
